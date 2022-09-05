469e7149a9bd753506c0fa9cfc2ba25b6643432a
skadrofske <skadrofs@purdue.edu>
2022-09-05 18:04:29 -0400

    accordion styles

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 window.addEventListener('load', (event) => {
     console.log('ready');
 });
+
+const accordion_panel = document.querySelectorAll(".accordion__panel");
+
+accordion_panel.forEach((a_panel) => {
+    a_panel.addEventListener("click", () => {
+        a_panel.classList.toggle("panel-open");
+    });
+});
